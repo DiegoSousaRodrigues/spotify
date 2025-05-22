feaf6df1143c13dff99b9966099451e0f0e498e7
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2025-05-22 08:18:47 -0300

    adicionando windstitches

--- src/components/LoginScreen/LoginScreen.styles.ts
+++ src/components/LoginScreen/LoginScreen.styles.ts
@@ -1,1 +1,5 @@
- 
+import { w } from 'windstitch'
+
+export const Wrapper = w.div(
+  'flex h-full items-center justify-center bg-linear-to-t from-[#111] to-[#222] bg-cover bg-no-repeat'
+)
--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -1,3 +1,27 @@
+import { Wrapper } from './LoginScreen.styles'
+
 export function LoginScreen() {
-  return <div>Login</div>
+  return (
+    <Wrapper>
+      <div className="h-fit bg-[#121212] px-[100px] py-[60px]">
+        <label className="flex gap-1 flex-col rounded-lg">
+          <span className="text-white font-semibold ">
+            E-mail ou nome de usuario
+          </span>
+          <input
+            type="text"
+            className="border rounded-sm border-white text-white p-2"
+          />
+        </label>
+
+        <label className="flex gap-1 flex-col bg-[#121212] px-[100px] py-[60px] rounded-lg">
+          <span className="text-white font-semibold ">Senha</span>
+          <input
+            type="text"
+            className="border rounded-sm border-white text-white p-2"
+          />
+        </label>
+      </div>
+    </Wrapper>
+  )
 }
